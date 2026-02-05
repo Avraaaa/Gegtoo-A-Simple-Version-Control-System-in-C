fb1ed676cdc40a15c091f25b3962301d451a6c52
Arithro <arithro.ch12@gmail.com>
2026-02-05 11:08:45 +0600

    feat: Implement geg checkout

--- src/main.c
+++ src/main.c
@@ -14,7 +14,6 @@
 #define PATH_MAX 4096
 #endif
 
-
 typedef struct
 {
     char *data;
@@ -23,20 +22,17 @@ typedef struct
     char id[41];
 } Blob;
 
-
 typedef struct
 {
     char *name;
     char *id;
 } Entry;
 
-
 typedef struct
 {
     Entry **entries;
     size_t count;
 } Tree;
-
 
 Entry *new_entry(char *name, char *id)
 {
@@ -46,7 +42,6 @@ Entry *new_entry(char *name, char *id)
     e->id = strdup(id);
     return e;
 }
-
 
 void free_entry(Entry *e)
 {
@@ -66,7 +61,6 @@ int compare_entries(const void *a, const void *b)
     return strcmp(ea->name, eb->name);
 }
 
-
 void hex_to_binary(const char *hex, unsigned char *out)
 {
 
@@ -75,7 +69,6 @@ void hex_to_binary(const char *hex, unsigned char *out)
         sscanf(hex + 2 * i, "%2hhx", &out[i]);
     }
 }
-
 
 unsigned char *serialize_tree(Tree *tree, size_t *out_size)
 {
@@ -110,7 +103,6 @@ unsigned char *serialize_tree(Tree *tree, size_t *out_size)
     *out_size = total_size;
     return buffer;
 }
-
 
 void create_directory(const char *path)
 {
@@ -152,7 +144,6 @@ void create_directory(const char *path)
         }
     };
 }
-
 
 void geg_init(const char *path)
 {
@@ -216,7 +207,6 @@ void geg_init(const char *path)
     printf("Initialized an empty geg repository in %s/\n", buffer);
 }
 
-
 int is_ignored(const char *name)
 {
 
@@ -237,7 +227,6 @@ int is_ignored(const char *name)
         return 0;
     }
 }
-
 
 char *read_workspace_files(const char *path, size_t *out_size)
 {
@@ -269,7 +258,6 @@ char *read_workspace_files(const char *path, size_t *out_size)
     *out_size = length;
     return buffer;
 }
-
 
 void database_store(Blob *blob)
 {
@@ -321,77 +309,77 @@ void database_store(Blob *blob)
     free(full_data);
 }
 
-
-int get_head_ref_path(char *ref_path_out){
+int get_head_ref_path(char *ref_path_out)
+{
 
     char head_path[PATH_MAX];
-    snprintf(head_path,sizeof(head_path),".geg/HEAD");
+    snprintf(head_path, sizeof(head_path), ".geg/HEAD");
 
-    FILE *fp = fopen(head_path,"r");
-    if(!fp) return -1;
+    FILE *fp = fopen(head_path, "r");
+    if (!fp)
+        return -1;
 
     char buffer[1024];
 
-    if(fscanf(fp,"ref: %s", buffer)!=1){
+    if (fscanf(fp, "ref: %s", buffer) != 1)
+    {
         fclose(fp);
         return -1;
     }
 
     fclose(fp);
 
-    snprintf(ref_path_out,PATH_MAX,".geg/%s",buffer);
-    
-    return 0;
+    snprintf(ref_path_out, PATH_MAX, ".geg/%s", buffer);
 
+    return 0;
 }
 
-
-char *get_parent_commit_id(){
+char *get_parent_commit_id()
+{
 
     char ref_path[PATH_MAX];
-    if(get_head_ref_path(ref_path)!=0) return NULL;
+    if (get_head_ref_path(ref_path) != 0)
+        return NULL;
 
-    FILE *fp = fopen(ref_path,"r");
-    if(!fp) return NULL;
+    FILE *fp = fopen(ref_path, "r");
+    if (!fp)
+        return NULL;
 
     char *commit_id = malloc(41);
-    if(fscanf(fp,"%40s",commit_id)!=1){
+    if (fscanf(fp, "%40s", commit_id) != 1)
+    {
 
         free(commit_id);
         fclose(fp);
         return NULL;
-
     }
 
     fclose(fp);
     return commit_id;
-
 }
 
-
-void update_head_ref(const char *new_commit_id){
+void update_head_ref(const char *new_commit_id)
+{
 
     char ref_path[PATH_MAX];
 
-    if(get_head_ref_path(ref_path)==0){
+    if (get_head_ref_path(ref_path) == 0)
+    {
 
-        FILE *fp = fopen(ref_path,"w");
-        if(fp){
+        FILE *fp = fopen(ref_path, "w");
+        if (fp)
+        {
 
-            fprintf(fp,"%s\n",new_commit_id);
+            fprintf(fp, "%s\n", new_commit_id);
             fclose(fp);
-
         }
-        else{
+        else
+        {
 
             perror("Failed to update HEAD ref");
-
         }
-
     }
-
 }
-
 
 char **list_workspace_files(const char *root_path, int *count)
 {
@@ -432,6 +420,120 @@ char **list_workspace_files(const char *root_path, int *count)
     return file_list;
 }
 
+void restore_blob(const char *path, const char *id)
+{
+    char obj_path[PATH_MAX];
+    char temp_path[PATH_MAX];
+    snprintf(obj_path, sizeof(obj_path), ".geg/objects/%.2s/%s", id, id + 2);
+    snprintf(temp_path, sizeof(temp_path), ".geg/temp_checkout_%s", id);
+
+    if (access(obj_path, F_OK) == -1)
+    {
+        printf("Error: Blob %s missing.\n", id);
+        return;
+    }
+
+    decompress(obj_path, temp_path);
+
+    FILE *src = fopen(temp_path, "rb");
+    if (!src)
+        return;
+
+    char type[10];
+    size_t size;
+    if (fscanf(src, "%s %zu", type, &size) != 2)
+    {
+        fclose(src);
+        return;
+    }
+    fgetc(src);
+
+    FILE *dst = fopen(path, "wb");
+    if (dst)
+    {
+        char buffer[1024];
+        size_t n;
+        while ((n = fread(buffer, 1, sizeof(buffer), src)) > 0)
+        {
+            fwrite(buffer, 1, n, dst);
+        }
+        fclose(dst);
+    }
+    else
+    {
+        perror("Failed to write workspace file");
+    }
+
+    fclose(src);
+    remove(temp_path);
+}
+
+void restore_tree(const char *tree_id, const char *base_path)
+{
+    char obj_path[PATH_MAX];
+    char temp_path[PATH_MAX];
+    snprintf(obj_path, sizeof(obj_path), ".geg/objects/%.2s/%s", tree_id, tree_id + 2);
+    snprintf(temp_path, sizeof(temp_path), ".geg/temp_tree_%s", tree_id);
+
+    if (access(obj_path, F_OK) == -1)
+        return;
+
+    decompress(obj_path, temp_path);
+    FILE *fp = fopen(temp_path, "rb");
+    if (!fp)
+        return;
+
+    char type[10];
+    size_t size;
+    if (fscanf(fp, "%s %zu", type, &size) != 2 || strcmp(type, "tree") != 0)
+    {
+        fclose(fp);
+        return;
+    }
+    fgetc(fp);
+
+    long data_start = ftell(fp);
+    while (ftell(fp) < data_start + size)
+    {
+        char mode[10];
+        char name[255];
+        unsigned char bin_sha[20];
+        char hex_sha[41];
+
+        if (fscanf(fp, "%s", mode) != 1)
+            break;
+        fgetc(fp);
+
+        int i = 0;
+        char c;
+        while ((c = fgetc(fp)) != '\0' && c != EOF && i < 254)
+            name[i++] = c;
+        name[i] = '\0';
+
+        if (fread(bin_sha, 1, 20, fp) != 20)
+            break;
+        for (int k = 0; k < 20; k++)
+            sprintf(hex_sha + (k * 2), "%02x", bin_sha[k]);
+
+        char full_path[PATH_MAX];
+        if (base_path && strlen(base_path) > 0)
+            snprintf(full_path, sizeof(full_path), "%s/%s", base_path, name);
+        else
+            snprintf(full_path, sizeof(full_path), "%s", name);
+
+        if (strcmp(mode, "100644") == 0)
+        {
+            restore_blob(full_path, hex_sha);
+        }
+        else
+        {
+            mkdir(full_path, 0755);
+            restore_tree(hex_sha, full_path);
+        }
+    }
+    fclose(fp);
+    remove(temp_path);
+}
 
 void geg_commit(void)
 {
@@ -638,53 +740,60 @@ void geg_cat(const char *id)
     remove(temp_path);
 }
 
-
-void geg_log(void){
+void geg_log(void)
+{
     char ref_path[PATH_MAX];
-    if(get_head_ref_path(ref_path)!=0){
+    if (get_head_ref_path(ref_path) != 0)
+    {
         printf("HEAD not found(not a geg repository?)\n");
         return;
     }
 
-    FILE *fp_ref = fopen(ref_path,"r");
+    FILE *fp_ref = fopen(ref_path, "r");
 
-    if(!fp_ref){
+    if (!fp_ref)
+    {
         printf("No Commits yet.\n");
         return;
     }
 
     char current_id[41];
-    if(fscanf(fp_ref,"%40s",current_id)!=1){
+    if (fscanf(fp_ref, "%40s", current_id) != 1)
+    {
         fclose(fp_ref);
         return;
     }
 
     fclose(fp_ref);
 
-    while(1){
+    while (1)
+    {
         char obj_path[PATH_MAX];
         char temp_path[PATH_MAX];
-        
+
         snprintf(obj_path, sizeof(obj_path), ".geg/objects/%.2s/%s", current_id, current_id + 2);
         snprintf(temp_path, sizeof(temp_path), ".geg/temp_log_%s", current_id);
-    
-        if(access(obj_path,F_OK) == -1){
+
+        if (access(obj_path, F_OK) == -1)
+        {
             printf("Error: Object %s is missing.\n", current_id);
             break;
         }
 
-        decompress(obj_path,temp_path);
+        decompress(obj_path, temp_path);
 
-        FILE *fp = fopen(temp_path,"rb");
+        FILE *fp = fopen(temp_path, "rb");
 
-        if(!fp){
+        if (!fp)
+        {
             perror("fopen");
             break;
         }
-        
+
         char type[10];
         size_t size;
-        if(fscanf(fp,"%s %zu", type, &size)!=2 || strcmp(type, "commit")){
+        if (fscanf(fp, "%s %zu", type, &size) != 2 || strcmp(type, "commit"))
+        {
             printf("Error: Object %s is not a commit.\n", current_id);
             fclose(fp);
             remove(temp_path);
@@ -697,19 +806,24 @@ void geg_log(void){
         char parent_id[41] = {0};
         char author[255] = {0};
 
-        while (fgets(line, sizeof(line), fp)){
+        while (fgets(line, sizeof(line), fp))
+        {
 
-            if(line[0]=='\n') break;
+            if (line[0] == '\n')
+                break;
 
-            if(strncmp(line,"parent ",7)==0){
+            if (strncmp(line, "parent ", 7) == 0)
+            {
                 sscanf(line, "parent %40s", parent_id);
             }
 
-            else if(strncmp(line, "author ", 7)==0){
-                char *p = line+7;
+            else if (strncmp(line, "author ", 7) == 0)
+            {
+                char *p = line + 7;
                 size_t len = strlen(p);
-                if(p[len-1]=='\n') p[len-1] = 0;
-                strcpy(author,p);
+                if (p[len - 1] == '\n')
+                    p[len - 1] = 0;
+                strcpy(author, p);
             }
         }
 
@@ -718,8 +832,9 @@ void geg_log(void){
 
         printf("\n");
 
-        while(fgets(line,sizeof(line),fp)){
-            printf("    %s",line);
+        while (fgets(line, sizeof(line), fp))
+        {
+            printf("    %s", line);
         }
         printf("\n");
 
@@ -727,16 +842,75 @@ void geg_log(void){
 
         remove(temp_path);
 
-        if(strlen(parent_id)>0){
-            strcpy(current_id,parent_id);
+        if (strlen(parent_id) > 0)
+        {
+            strcpy(current_id, parent_id);
         }
 
-        else{
+        else
+        {
             break;
         }
     }
 }
 
+void geg_checkout(const char *target_id)
+{
+
+    char obj_path[PATH_MAX];
+    snprintf(obj_path, sizeof(obj_path), ".geg/objects/%.2s/%s", target_id, target_id + 2);
+
+    if (access(obj_path, F_OK) == -1)
+    {
+
+        printf("Commit %s not found.\n", target_id);
+        return;
+    }
+
+    char temp_path[PATH_MAX];
+    snprintf(temp_path, sizeof(temp_path), ".geg/temp_chk_%s", target_id);
+    decompress(obj_path, temp_path);
+
+    FILE *fp = fopen(temp_path, "rb");
+    if (!fp)
+        return;
+
+    char type[10];
+    size_t size;
+    fscanf(fp, "%s %zu", type, &size);
+    fgetc(fp);
+
+    char line[1024];
+    char tree_id[41] = {0};
+    if (fgets(line, sizeof(line), fp))
+    {
+        if (strncmp(line, "tree ", 5) == 0)
+            sscanf(line, "tree %40s", tree_id);
+    }
+
+    fclose(fp);
+    remove(temp_path);
+
+    if (strlen(tree_id) == 0)
+    {
+
+        printf("Invalid commit object(no tree found).\n");
+        return;
+    }
+
+    printf("Checking out commit %s (tree %s)...\n", target_id, tree_id);
+    restore_tree(tree_id, "");
+
+
+    FILE *head = fopen(".geg/HEAD","w");
+
+    if(head){
+
+        fprintf(head,"%s\n",target_id);
+        fclose(head);
+
+    }
+}
 
 int main(int argc, char *argv[])
 {
@@ -780,6 +954,14 @@ int main(int argc, char *argv[])
     else if (strcmp(command, "log") == 0)
     {
         geg_log();
+    }
+
+    else if (strcmp(command, "checkout") == 0)
+    {
+        if (argc < 3)
+            printf("Usage: ./geg checkout <commit_id>\n");
+        else
+            geg_checkout(argv[2]);
     }
 
     else
